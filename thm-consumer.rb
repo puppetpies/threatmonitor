42bd71e883d560151190c0fcf8bab5ccbff33969
Bri in The Sky <brianh6854@googlemail.com>
2015-06-21 20:14:00 +0100

    Added Infinite consumer or one that runs so many passes functionality / getopt code

--- thm-consumer.rb
+++ thm-consumer.rb
@@ -8,14 +8,49 @@
 #
 ################################################### #####################
 
+require 'getoptlong'
 require './thmmq.rb'
+
+ARGV[0] = "--help" if ARGV[0] == nil
 
 banner = "\e[1;34mWelcome to Threatmonitor Consumer \e[0m\ \n"
 banner << "\e[1;34m=================================\e[0m\ \n"
-puts banner
-      
-# See thmmq.rb for list for variables
 
+opts = GetoptLong.new(
+  [ '--help', '-h', GetoptLong::NO_ARGUMENT ],
+  [ '--infinite', '-i', GetoptLong::NO_ARGUMENT ],
+  [ '--passes', '-p', GetoptLong::REQUIRED_ARGUMENT ]
+)
+
+opts.each do |opt, arg|
+  case opt
+    when '--help'
+      helper = %q[
+-h, --help:
+   show help
+   
+-i, --infinite [ For consuming live captures ]
+  
+-p, --passes number of passes  [ For medium / larger queues ]
+
+NOTES:
+
+  Press CTRL+C to exit Reactor thread at anytime to move on to next queue.
+  There are currently 3 queues ippacket, tcppacket, udppacket
+  
+    ]
+      puts banner
+      puts helper
+      exit
+    when '--infinite'
+      @infinite = arg
+    when '--passes'
+      @passes = arg
+  end
+end
+
+puts banner
+# See thmmq.rb for list for variables
 obj = Thm::Consumer.new
 obj.queueprefix = "wifi"
 obj.tblname_ippacket = "wifi_ippacket"
@@ -24,7 +59,12 @@ obj.tblname_udppacket = "wifi_udppacket"
 obj.mqconnect
 obj.dbconnect
 # Send data to database from queue
-obj.from_mq_to_db
-
+if @infinite.nil? == false
+  obj.infinite
+elsif @passes.nil? == false
+  obj.passes(@passes.to_i)
+else
+  obj.from_mq_to_db
+end
 obj.mqclose
 
