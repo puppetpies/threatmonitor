657e9acb96a3f50e51f3a32cdf5f49b31c9197d5
Bri in The Sky <brianh6854@googlemail.com>
2015-06-23 13:27:08 +0100

    Added Localmachine class for PCap file loading / Bug fixes with for Trueclass / work around

--- thmmq.rb
+++ thmmq.rb
@@ -19,6 +19,11 @@ require 'pcaplet'
 require './datalayerlight.rb'
 include Pcap
 
+# TODO
+#
+# Create def's for that packet SQL / Refactor to provent code duplication
+# Create def's for Hash table YAML same idea as above.
+
 trap("INT") {
   
   if EM.reactor_running? == true
@@ -319,7 +324,8 @@ module Thm
             ip_packet << "VALUES ("
             ip_packet << "'#{ipdatadim["guid"]}',"
             ip_packet << "'#{ipdatadim["recv_date"]}',"
-            if @ip_df == true 
+            ip_df = ipdatadim["ip_df"].to_s # Due to TrueClass issues will have a look later
+            if ip_df == "true" 
               ip_packet << "'Y',"
             else
               ip_packet << "'N',"
@@ -328,7 +334,8 @@ module Thm
             ip_packet << "'#{ipdatadim["ip_hlen"]}',"
             ip_packet << "'#{ipdatadim["ip_id"]}',"
             ip_packet << "'#{ipdatadim["ip_len"]}',"
-            if @ip_mf == true
+            ip_mf = ipdatadim["ip_mf"].to_s
+            if ip_mf == "true"
               ip_packet << "'Y',"
             else
               ip_packet << "'N',"
@@ -341,7 +348,7 @@ module Thm
             ip_packet << "'#{ipdatadim["ip_ttl"]}',"
             ip_packet << "'#{ipdatadim["ip_ver"]}');"
             if t == 50
-              puts "\e[1;32m\ MSGID:\e[0m\ [#{n}] \e[1;32m\Generated SQL:\e[0m\ #{ip_packet}"
+              puts "\e[1;32m\ DF: #{ip_df} MF: #{ip_mf} MSGID:\e[0m\ [#{n}] \e[1;32m\Generated SQL:\e[0m\ #{ip_packet}"
               t = 0
             end
             t = t + 1 unless t == 50
@@ -380,32 +387,38 @@ module Thm
           tcp_packet << "'#{tcpdatadim["recv_date"]}', "
           tcp_packet << "#{tcpdatadim["tcp_data_len"]},"
           tcp_packet << "#{tcpdatadim["tcp_dport"]},"
-          if tcpdatadim["tcp_ack"] == "true" 
+          tcp_ack = tcpdatadim["tcp_ack"].to_s
+          if tcp_ack == "true" 
             tcp_packet << "'Y',"
           else
             tcp_packet << "'N',"
           end
-          if tcpdatadim["tcp_fin"] == "true"
+          tcp_fin = tcpdatadim["tcp_fin"].to_s
+          if tcp_fin == "true"
             tcp_packet << "'Y',"
           else
             tcp_packet << "'N',"
           end
-          if tcpdatadim["tcp_syn"] == "true" 
+          tcp_syn = tcpdatadim["tcp_syn"].to_s
+          if tcp_syn == "true" 
             tcp_packet << "'Y',"
           else
             tcp_packet << "'N',"
           end
-          if tcpdatadim["tcp_rst"] == "true" 
+          tcp_rst = tcpdatadim["tcp_rst"].to_s
+          if tcp_rst == "true" 
             tcp_packet << "'Y',"
           else
             tcp_packet << "'N',"
           end
-          if tcpdatadim["tcp_psh"] == "true" 
+          tcp_psh = tcpdatadim["tcp_psh"].to_s
+          if tcp_psh == "true" 
             tcp_packet << "'Y',"
           else
             tcp_packet << "'N',"
           end
-          if tcpdatadim["tcp_urg"] == "true"
+          tcp_urg = tcpdatadim["tcp_urg"].to_s
+          if tcp_urg == "true"
             tcp_packet << "'Y',"
           else
             tcp_packet << "'N',"
@@ -497,4 +510,141 @@ module Thm
     
   end
 
+  # Process data from / to local files.
+  
+  class Localmachine < DataServices
+  
+    def from_pcap_db(pcapfile)
+      t, n, s, v, x, z = 0, 0, 0, 0, 0, 0
+      ipcount, tcpcount, udpcount = 0, 0, 0
+      inp = Pcap::Capture.open_offline(pcapfile)
+      inp.each_packet do |pkt|
+        guid = Tools::guid # IP / TCP / UDP relationship
+        dtime = Time.now
+        # IP Packet
+        if pkt.ip?
+            ip_packet = "INSERT INTO \"#@dbname\".#{@tblname_ippacket} "
+            ip_packet << "(guid, recv_date, ip_df, ip_dst, ip_hlen, ip_id, ip_len, ip_mf, ip_off, ip_proto, ip_src, ip_sum, ip_tos, ip_ttl, ip_ver) "
+            ip_packet << "VALUES ("
+            ip_packet << "'#{guid}',"
+            ip_packet << "'#{dtime}',"
+            if pkt.ip_df? == true 
+              ip_packet << "'Y',"
+            else
+              ip_packet << "'N',"
+            end
+            ip_packet << "'#{pkt.ip_dst}',"
+            ip_packet << "'#{pkt.ip_hlen}',"
+            ip_packet << "'#{pkt.ip_id}',"
+            ip_packet << "'#{pkt.ip_len}',"
+            if pkt.ip_mf? == true
+              ip_packet << "'Y',"
+            else
+              ip_packet << "'N',"
+            end
+            ip_packet << "'#{pkt.ip_off}',"
+            ip_packet << "'#{pkt.ip_proto}',"
+            ip_packet << "'#{pkt.ip_src}',"
+            ip_packet << "'#{pkt.ip_sum}',"
+            ip_packet << "'#{pkt.ip_tos}',"
+            ip_packet << "'#{pkt.ip_ttl}',"
+            ip_packet << "'#{pkt.ip_ver}');"
+            if t == 50
+              puts "\e[1;32m\ MSGID:\e[0m\ [#{n}] \e[1;32m\Generated SQL:\e[0m\ #{ip_packet}"
+              t = 0
+            end
+            t = t + 1 unless t == 50
+            res = @conn.query("#{ip_packet}")
+            @conn.save
+            n = n + 1
+        end
+        # TCP Packet
+        if pkt.tcp?
+          tcp_packet = "INSERT INTO \"#@dbname\".#{@tblname_tcppacket} "
+          tcp_packet << "(guid, recv_date, tcp_data_len, tcp_dport, tcp_ack, tcp_fin, tcp_syn, tcp_rst, tcp_psh, tcp_urg, tcp_off, tcp_hlen, tcp_seq, tcp_sum, tcp_sport, tcp_urp, "
+          tcp_packet << "tcp_win) "
+          tcp_packet << "VALUES ("
+          tcp_packet << "'#{guid}',"
+          tcp_packet << "'#{dtime}', "
+          tcp_packet << "#{pkt.tcp_data_len},"
+          tcp_packet << "#{pkt.tcp_dport},"
+          if pkt.tcp_ack? == true 
+            tcp_packet << "'Y',"
+          else
+            tcp_packet << "'N',"
+          end
+          if pkt.tcp_fin? == true
+            tcp_packet << "'Y',"
+          else
+            tcp_packet << "'N',"
+          end
+          if pkt.tcp_syn? == true 
+            tcp_packet << "'Y',"
+          else
+            tcp_packet << "'N',"
+          end
+          if pkt.tcp_rst? == true 
+            tcp_packet << "'Y',"
+          else
+            tcp_packet << "'N',"
+          end
+          if pkt.tcp_psh? == true 
+            tcp_packet << "'Y',"
+          else
+            tcp_packet << "'N',"
+          end
+          if pkt.tcp_urg? == true
+            tcp_packet << "'Y',"
+          else
+            tcp_packet << "'N',"
+          end
+          tcp_packet << "#{pkt.tcp_off}, #{pkt.tcp_hlen}, #{pkt.tcp_seq}, #{pkt.tcp_sum}, #{pkt.tcp_sport}, #{pkt.tcp_urp}, #{pkt.tcp_win});"
+          if s == 50
+            puts "\e[1;32m\ MSGID:\e[0m\ [#{v}] \e[1;32m\Generated SQL:\e[0m\ #{tcp_packet}"
+            s = 0
+          end
+          s = s + 1 unless s == 50
+          res = @conn.query("#{tcp_packet}")
+          @conn.save
+          v = v + 1        
+        end
+        # UDP Packet
+        if pkt.udp?
+          udp_packet = "INSERT INTO \"#@dbname\".#{@tblname_udppacket} "
+          udp_packet << "(guid,"
+          udp_packet << "recv_date,"
+          udp_packet << "udp_dport,"
+          udp_packet << "udp_len,"
+          udp_packet << "udp_sum,"
+          udp_packet << "udp_sport) "
+          udp_packet << "VALUES ("
+          udp_packet << "'#{guid}',"
+          udp_packet << "'#{dtime}',"
+          udp_packet << "'#{pkt.udp_dport}',"
+          udp_packet << "'#{pkt.udp_len}',"
+          udp_packet << "'#{pkt.udp_sum}',"
+          udp_packet << "'#{pkt.udp_sport}');"
+          if x == 50
+            puts "\e[1;32m\ MSGID:\e[0m\ [#{z}] \e[1;32m\Generated SQL:\e[0m\ #{udp_packet}"
+            x = 0
+          end
+          x = x + 1 unless x == 50
+          res = @conn.query("#{udp_packet}")
+          @conn.save
+          z = z + 1        
+        end
+        ipcount = n
+        tcpcount = v
+        udpcount = z
+      end
+      @conn.release
+      @conn.commit
+      totals = "\e[1;31m=======================================================================\e[0m\ \n"
+      totals << "\e[1;31mPackets Total | IP: #{ipcount} | TCP: #{tcpcount} | UDP: #{udpcount}\e[0m\ \n"
+      totals << "\e[1;31m======================================================================\e[0m\ \n"
+      puts totals
+    end
+    
+  end
+  
 end
