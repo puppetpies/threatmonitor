8e49696eb39c4960db05458c8603932f57b7e569
Bri in The Sky <brianh6854@googlemail.com>
2015-07-02 21:54:51 +0100

    Initial Commit

---
+++ thm-authentication.rb
@@ -0,0 +1,20 @@
+########################################################################
+#
+# Author: Brian Hood
+#
+# Description: Threatmonitor User Administration
+# 
+# Extends the functionality of the Thm module adding Authorization
+# Adding Authentication to the Privileges model
+#
+########################################################################
+
+require './thm-authorization.rb'
+
+module Thm::Authorization
+
+  class Authentication
+  
+  end
+
+end
