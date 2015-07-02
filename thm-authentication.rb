d04dbea89cdbd7be30cadab7226579510ddf903c
Bri in The Sky <brianh6854@googlemail.com>
2015-07-03 00:27:55 +0100

    Added login / login_status? / logout session functionality

--- thm-authentication.rb
+++ thm-authentication.rb
@@ -9,12 +9,42 @@
 #
 ########################################################################
 
-require './thm-authorization.rb'
+require './thm-privileges.rb'
 
 module Thm::Authorization
 
-  class Authentication
-  
+  class Authentication < Thm::DataServices
+    
+    attr_reader :thmsession
+    
+    def login(username, password)
+      obj = Thm::Authorization::Privileges.new
+      pwhash = obj.mkhash(password)
+      sqlusrcnt = "SELECT count(*) as num FROM users WHERE username = '#{username}' and password '#{pwhash}';"
+      resusrcnt = @conn.query("#{sqlusrcnt}")
+      rowusrcnt = resusrcnt.fetch_hash
+      puts "#{rowusrcnt["num"].to_i}"
+      if rowusrcnt["num"].to_i == 1
+        puts "Authentication Success"
+        @thmsession = Tools::guid
+      else
+        @thmsession = "failure"
+        puts "Failure to Authenticate"
+      end
+    end
+    
+    def login_status?
+      if @thmsession != "failure" or @thmsession != nil
+        return true
+      else
+        return false
+      end 
+    end
+    
+    def logout
+      @thmsession = nil
+    end
+    
   end
 
 end
