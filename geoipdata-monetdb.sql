99cf7247bd5daa71cfab940e58f68028ce858d7a
Bri in The Sky <brianh6854@googlemail.com>
2015-07-03 19:10:35 +0100

    Initial Commit

---
+++ geoipdata-monetdb.sql
@@ -0,0 +1,38 @@
+-- http://dev.maxmind.com/geoip/geoip2/geolite2/
+
+CREATE SCHEMA "geoipdata";
+
+DROP TABLE "geoipdata".ipv4blocks;
+CREATE TABLE "geoipdata".ipv4blocks (
+id INT GENERATED ALWAYS AS 
+        IDENTITY (
+           START WITH 1 INCREMENT BY 1
+           NO MINVALUE NO MAXVALUE
+           CACHE 2 CYCLE
+) primary key,
+  network varchar(18),
+  geoname_id int not null,
+  registered_country_geoname_id int not null,
+  represented_country_geoname_id int not null,
+  is_anonymous_proxy char(1),
+  is_satellite_provider char(1),
+  UNIQUE (geoname_id)
+);
+
+DROP TABLE "geoipdata".locations;
+CREATE TABLE "geoipdata".locations (
+id INT GENERATED ALWAYS AS 
+        IDENTITY (
+           START WITH 1 INCREMENT BY 1
+           NO MINVALUE NO MAXVALUE
+           CACHE 2 CYCLE
+) primary key,
+  geoname_id int not null,
+  locale_code char(2),
+  continent_code char(2),
+  continent_name char(15),
+  country_iso_code char(2),
+  country_name char(50),
+  FOREIGN KEY (geoname_id) REFERENCES "geoipdata".ipv4blocks (geoname_id)
+);
+
