3503116c50f9ae2d6e5f50ade8b8b5f47f33a50b
Bri in The Sky <brianh6854@googlemail.com>
2015-07-05 13:41:17 +0100

    Fixed GeoIPData import for Country

--- geoipdata-monetdb.sql
+++ geoipdata-monetdb.sql
@@ -1,42 +1,38 @@
 -- http://dev.maxmind.com/geoip/geoip2/geolite2/
 
-CREATE SCHEMA "geoipdata";
+-- id INT GENERATED ALWAYS AS 
+--        IDENTITY (
+--           START WITH 1 INCREMENT BY 1
+--           NO MINVALUE NO MAXVALUE
+--           CACHE 2 CYCLE
+-- ) primary key,
 
-DROP TABLE "geoipdata".ipv4blocks;
-CREATE TABLE "geoipdata".ipv4blocks (
-id INT GENERATED ALWAYS AS 
-        IDENTITY (
-           START WITH 1 INCREMENT BY 1
-           NO MINVALUE NO MAXVALUE
-           CACHE 2 CYCLE
-) primary key,
+DROP TABLE "threatmonitor".geoipdata_ipv4blocks_country;
+CREATE TABLE "threatmonitor".geoipdata_ipv4blocks_country (
   network varchar(18),
-  geoname_id int not null,
-  registered_country_geoname_id int not null,
-  represented_country_geoname_id int not null,
-  is_anonymous_proxy char(1),
-  is_satellite_provider char(1),
-  UNIQUE (geoname_id)
+  geoname_id char(10),
+  registered_country_geoname_id char(30),
+  represented_country_geoname_id char(30),
+  is_anonymous_proxy char(30),
+  is_satellite_provider char(30)
 );
 
-DROP TABLE "geoipdata".locations;
-CREATE TABLE "geoipdata".locations (
-id INT GENERATED ALWAYS AS 
-        IDENTITY (
-           START WITH 1 INCREMENT BY 1
-           NO MINVALUE NO MAXVALUE
-           CACHE 2 CYCLE
-) primary key,
-  geoname_id int not null,
+CREATE INDEX index_ipv4_network ON "threatmonitor".geoipdata_ipv4blocks_country(network);
+CREATE INDEX index_ipv4_geoname_id ON "threatmonitor".geoipdata_ipv4blocks_country(geoname_id);
+COPY 169357 OFFSET 2 RECORDS INTO "threatmonitor".geoipdata_ipv4blocks_country FROM '/data2/MaxMind/GeoLite2-Country-CSV_20150602/GeoLite2-Country-Blocks-IPv4.csv' USING DELIMITERS ',', '\n', '';
+
+DROP TABLE "threatmonitor".geoipdata_locations_country;
+CREATE TABLE "threatmonitor".geoipdata_locations_country (
+  geoname_id bigint not null,
   locale_code char(2),
   continent_code char(2),
   continent_name char(15),
   country_iso_code char(2),
   country_name char(50)
--- FOREIGN KEY (geoname_id) REFERENCES "geoipdata".ipv4blocks (geoname_id)
+-- FOREIGN KEY (geoname_id) REFERENCES "geoipdata".geoipdata_ipv4blocks_country (index_geoname_id)
 );
 
-COPY 169357 OFFSET 1 RECORDS INTO "geoipdata".ipv4blocks FROM '/data2/MaxMind/GeoLite2-Country-Blocks-IPv4.csv' USING DELIMITERS ',','\n','';
-COPY 250 OFFSET 1 RECORDS INTO "geoipdata".locations FROM '/data2/MaxMind/GeoLite2-Country-CSV_20150602/en.csv' USING DELIMITERS ',','\n','';
+CREATE INDEX index_country_geoname_id ON "threatmonitor".geoipdata_locations_country(geoname_id);
+COPY 250 OFFSET 2 RECORDS INTO "threatmonitor".geoipdata_locations_country FROM '/data2/MaxMind/GeoLite2-Country-CSV_20150602/GeoLite2-Country-Locations-en.csv' USING DELIMITERS ',', '\n', '';
 
 
