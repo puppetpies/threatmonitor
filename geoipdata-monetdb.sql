2e84dd5933a60f9435c02f55d3ab679bd5f99ab8
Bri in The Sky <brianh6854@googlemail.com>
2015-07-05 14:47:00 +0100

    Added example JOIN

--- geoipdata-monetdb.sql
+++ geoipdata-monetdb.sql
@@ -23,7 +23,7 @@ COPY 169357 OFFSET 2 RECORDS INTO "threatmonitor".geoipdata_ipv4blocks_country F
 
 DROP TABLE "threatmonitor".geoipdata_locations_country;
 CREATE TABLE "threatmonitor".geoipdata_locations_country (
-  geoname_id bigint not null,
+  geoname_id char(10),
   locale_code char(2),
   continent_code char(2),
   continent_name char(15),
@@ -34,5 +34,27 @@ CREATE TABLE "threatmonitor".geoipdata_locations_country (
 
 CREATE INDEX index_country_geoname_id ON "threatmonitor".geoipdata_locations_country(geoname_id);
 COPY 250 OFFSET 2 RECORDS INTO "threatmonitor".geoipdata_locations_country FROM '/data2/MaxMind/GeoLite2-Country-CSV_20150602/GeoLite2-Country-Locations-en.csv' USING DELIMITERS ',', '\n', '';
+
+plan SELECT network, locale_code, continent_code, continent_name, country_name, country_iso_code 
+FROM "threatmonitor".geoipdata_ipv4blocks_country a 
+JOIN "threatmonitor".geoipdata_locations_country b 
+ON (a.geoname_id = b.geoname_id) 
+WHERE network LIKE '23.%' 
+GROUP BY a.network, b.locale_code, b.continent_code, b.continent_name, b.country_name, b.country_iso_code
+LIMIT 100;
+
+-- SELECT network FROM "threatmonitor".geoipdata_ipv4blocks_country a JOIN WHERE network LIKE '23.%' LIMIT 5;
+ 
+-- PLAN SELECT LEFT(network, 8) as net, locale_code, continent_code, continent_name, country_name, country_iso_code 
+-- FROM "threatmonitor".geoipdata_ipv4blocks_country a 
+-- JOIN "threatmonitor".geoipdata_locations_country b 
+-- ON (a.geoname_id = b.geoname_id) 
+-- JOIN "threatmonitor".wifi_ippacket c
+-- ON (c.ip_dst LIKE LEFT(network, 8))
+-- JOIN "threatmonitor".wifi_tcppacket d
+-- ON (c.guid = d.guid)
+-- WHERE network LIKE '23.%' 
+-- GROUP BY a.network, b.locale_code, b.continent_code, b.continent_name, b.country_name, b.country_iso_code
+-- LIMIT 100;
 
 
