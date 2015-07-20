fa246734d0cdbf30bb1100d7414616ed0f6d6d49
Bri in The Sky <brianh6854@googlemail.com>
2015-07-20 15:12:49 +0100

    Added InfluxDB class / RestAPI

--- lib/thm/datalayerlight.rb
+++ lib/thm/datalayerlight.rb
@@ -82,6 +82,7 @@ module DatalayerLight
 
   end
 
+  # Fine for small capture projects
   class MySQLDrv
     
     require 'mysql'
@@ -139,5 +140,73 @@ module DatalayerLight
     end
     
   end
+
+  # Metrics / Measurements Engine InfluxDB RestAPI
+  class InfluxDB
+
+    require "net/http"
+    require "uri"
+    require "json"
+    require "pp"
   
+    attr_accessor :dbhost, :dbuser, :dbpass, :dbport, :dburl
+
+    def initialize
+      @dbhost = "127.0.0.1"
+      @dbuser = "threatmonitor"
+      @dbpass = "dk3rbi9l"
+      @dbport = 8086
+      @dbname = "threatmonitor"
+    end
+    
+    def apiget(sql)
+      @dburl = "http://#{@dbhost}:#{@dbport}"
+      sqlunicode = URI.encode(sql)
+      puts "InfluxDB SQL URL: #{@dburl}/query?db=#{@dbname}&q=#{sqlunicode}"
+      uri = URI.parse("#{@dburl}/query?db=#{@dbname}&q=#{sqlunicode}")
+      puts "Request URI: #{uri}"
+      http = Net::HTTP.new(uri.host, uri.port)
+      begin
+        response = http.request(Net::HTTP::Get.new(uri.request_uri))
+        begin
+          j = JSON.parse(response.body)
+        rescue JSON::ParserError
+          puts "Could not read JSON data"
+        end
+      rescue
+        puts "Error retrieving data"
+      end
+    end
+    
+    def apipost(data)
+      @dburl = "http://#{@dbhost}:#{@dbport}"
+      #puts "InfluxDB SQL URL: #{@dburl}/query?db=#{@dbname}"
+      uri = URI.parse("#{@dburl}/write?db=#{@dbname}")
+      #puts "Request URI: #{uri}"
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Post.new(uri.request_uri)
+      request.set_content_type("application/x-www-form-urlencoded")
+      begin
+        request.body = data unless data.empty?
+        response = http.request(request)
+        if response.code == 204 # Good response
+          puts "OK"
+        elsif response.code == 200 or response.code == 400 # 200 can be an error in some cases !!
+          puts "Error code #{response.code}"
+        end
+      rescue
+        puts "Error posting data"
+      end
+    end
+    
+    def query(sql, mode="r")
+      if mode == "r"
+        apiget("#{sql}")
+      elsif mode == "w"
+        apipost(sql)
+      end
+    end
+    
+  end
+
 end
