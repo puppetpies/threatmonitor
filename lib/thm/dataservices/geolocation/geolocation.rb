bded3c0507faed6c198cfddfef7de88a2102346e
Bri in The Sky <brianh6854@googlemail.com>
2015-07-31 19:47:22 +0100

    Added Geo Query Code / Debug

---
+++ lib/thm/dataservices/geolocation/geolocation.rb
@@ -0,0 +1,31 @@
+module Thm
+
+  class DataServices::Geolocation < DataServices
+  
+    attr_writer :geodebug
+    
+    def initialize
+      @geodebug = true
+    end
+
+    def geoiplookup(ip)
+      geoquery = "SELECT continent_name, country_name FROM geoipdata_ipv4blocks_country a JOIN geoipdata_locations_country b ON (a.geoname_id = b.geoname_id) WHERE network LIKE '#{ip.split(".")[0]}.#{ip.split(".")[1]}.%' GROUP BY b.continent_name, b.country_name LIMIT 1;"
+      if @geodebug == true
+        puts "Geo Query: #{geoquery}"
+      end
+      resusrcnt = @conn.query("#{geoquery}")
+      while row = resusrcnt.fetch_hash do
+        continent_name = row["continent_name"].to_s
+        country_name = row["country_name"].to_s
+        #if continent_name.strip == "" and country_name.strip != ""
+          res = "(#{country_name})"
+        #elsif continent_name.strip != "" and country_name.strip == ""
+        #  res = "(#{continent_name})"
+        #end
+      end
+      return res
+    end
+
+  end
+
+end
