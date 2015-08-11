34b50bd0be9544af82a0d03553456b23f2ccd9fd
Bri in The Sky <brianh6854@googlemail.com>
2015-08-11 15:44:44 +0100

    Need to squash bug but better

--- lib/thm/dataservices/geolocation/geolocation.rb
+++ lib/thm/dataservices/geolocation/geolocation.rb
@@ -1,3 +1,5 @@
+require 'pp'
+
 module Thm
 
   class DataServices::Geolocation < DataServices
@@ -6,41 +8,47 @@ module Thm
     
     def initialize
       @geodebug = false
+      @continent_name = String.new
+      @country_name = String.new
+      @city_name = String.new
     end
     
     def self.define_component(name)
       name_func = name.to_sym
-      define_method(name_func) do 
+      define_method(name_func) do |ip|
         geoquery = "SELECT count(*) as num FROM geoipdata_ipv4blocks_#{name_func} a "
         geoquery << "JOIN geoipdata_locations_#{name_func} b ON (a.geoname_id = b.geoname_id) "
         geoquery << "WHERE network LIKE '#{ip.split(".")[0]}.#{ip.split(".")[1]}.%' GROUP BY b.continent_name, b.#{name_func}_name LIMIT 1;"
         res = @conn.query("#{geoquery}")
-        row = res.fetch_hash
-        num = row["num"].to_i
-        if num == 0;
-          return false
-        else
+        rowgeocount = res.fetch_hash
+        begin
+          geocount = rowgeocount["num"]
+        rescue => e
+          pp e
+        end
+        puts "Geo SELECT COUNT: #{geoquery}: Number: #{geocount}" # if @geodebug == true
+        if geocount > 0;
           geoquery = "SELECT continent_name, #{name_func}_name FROM geoipdata_ipv4blocks_#{name_func} a "
           geoquery << "JOIN geoipdata_locations_#{name_func} b ON (a.geoname_id = b.geoname_id) "
           geoquery << "WHERE network LIKE '#{ip.split(".")[0]}.#{ip.split(".")[1]}.%' GROUP BY b.continent_name, b.#{name_func}_name LIMIT 1;"
+          puts "Geo SELECT: #{geoquery}" if @geodebug == true
+          resgeo = @conn.query("#{geoquery}")
+          row = resgeo.fetch_hash
+          @continent_name = row["continent_name"].to_s
+          instance_variable_set("@#{name_func}_name", row["#{name_func}_name"].to_s)
+        else
+          return false
         end
       end
     end
 
-    define_component :country?
-    define_component :city?
+    define_component :country
+    define_component :city
   
     def geoiplookup(ip)
-      geoquery = country? | city?
-      if @geodebug == true
-        puts "Geo Query: #{geoquery}"
-      end
-      resusrcnt = @conn.query("#{geoquery}")
-      while row = resusrcnt.fetch_hash do
-        continent_name = row["continent_name"].to_s
-        country_name = row["country_name"].to_s
-        res = "(#{country_name})"
-      end
+      country(ip)
+      city(ip)
+      res = "( #{@country_name} - (#{@continent_name}) #{@city_name})"
       return res
     end
 
