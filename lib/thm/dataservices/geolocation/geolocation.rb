8699963b23d0595dca6314bdc57ec8061e422262
Bri in The Sky <brianh6854@googlemail.com>
2015-08-11 18:19:51 +0100

    Removed Aggregate function from count(*) stupid / Added Error handling and / New Output formatting

--- lib/thm/dataservices/geolocation/geolocation.rb
+++ lib/thm/dataservices/geolocation/geolocation.rb
@@ -8,9 +8,9 @@ module Thm
     
     def initialize
       @geodebug = false
-      @continent_name = String.new
-      @country_name = String.new
-      @city_name = String.new
+      @continent_name = Array.new
+      @country_name = Array.new
+      @city_name = Array.new
     end
     
     def self.define_component(name)
@@ -18,24 +18,31 @@ module Thm
       define_method(name_func) do |ip|
         geoquery = "SELECT count(*) as num FROM geoipdata_ipv4blocks_#{name_func} a "
         geoquery << "JOIN geoipdata_locations_#{name_func} b ON (a.geoname_id = b.geoname_id) "
-        geoquery << "WHERE network LIKE '#{ip.split(".")[0]}.#{ip.split(".")[1]}.%' GROUP BY b.continent_name, b.#{name_func}_name LIMIT 1;"
-        res = @conn.query("#{geoquery}")
-        rowgeocount = res.fetch_hash
+        geoquery << "WHERE network LIKE '#{ip.split(".")[0]}.#{ip.split(".")[1]}.%';"
         begin
-          geocount = rowgeocount["num"]
+          res = @conn.query("#{geoquery}")
+          rowgeocount = res.fetch_hash
+          geocount = rowgeocount["num"].to_i
         rescue => e
           pp e
         end
-        puts "Geo SELECT COUNT: #{geoquery}: Number: #{geocount}" # if @geodebug == true
+        puts "Geo SELECT COUNT: #{geoquery}: Number: #{geocount}" if @geodebug == true
         if geocount > 0;
           geoquery = "SELECT continent_name, #{name_func}_name FROM geoipdata_ipv4blocks_#{name_func} a "
           geoquery << "JOIN geoipdata_locations_#{name_func} b ON (a.geoname_id = b.geoname_id) "
           geoquery << "WHERE network LIKE '#{ip.split(".")[0]}.#{ip.split(".")[1]}.%' GROUP BY b.continent_name, b.#{name_func}_name LIMIT 1;"
           puts "Geo SELECT: #{geoquery}" if @geodebug == true
-          resgeo = @conn.query("#{geoquery}")
-          row = resgeo.fetch_hash
-          @continent_name = row["continent_name"].to_s
-          instance_variable_set("@#{name_func}_name", row["#{name_func}_name"].to_s)
+          begin
+            resgeo = @conn.query("#{geoquery}")
+            while row = resgeo.fetch_hash do
+              populategeo = instance_variable_get("@#{name_func}_name")
+              populategeo << row["#{name_func}_name"].to_s
+              instance_variable_set("@#{name_func}_name", populategeo)
+              @continent_name = row["continent_name"].to_s
+            end
+          rescue => e
+            pp e
+          end
         else
           return false
         end
@@ -44,11 +51,22 @@ module Thm
 
     define_component :country
     define_component :city
-  
+
     def geoiplookup(ip)
-      country(ip)
+      t = country(ip)
       city(ip)
-      res = "( #{@country_name} - (#{@continent_name}) #{@city_name})"
+      unless t == false
+        res = "(#{@continent_name}) - \n"
+        @country_name.each {|n|
+          res << "[ #{n} ]"
+        }
+        @city_name.each {|n|
+          res << "[ #{n} ] "
+        }
+        initialize
+      else
+        res = "Not Available"
+      end
       return res
     end
 
