ea0775a65e033580aaf23230b0c2b1ce13c42d3b
Bri in The Sky <brianh6854@googlemail.com>
2015-08-11 18:30:03 +0100

    Made fields not null for database tables

--- sql/geoipdata-monetdb.sql
+++ sql/geoipdata-monetdb.sql
@@ -9,15 +9,15 @@
 
 DROP TABLE "threatmonitor".geoipdata_ipv4blocks_city;
 CREATE TABLE "threatmonitor".geoipdata_ipv4blocks_city (
-  network varchar(18),
-  geoname_id char(10),
-  registered_country_geoname_id char(30),
-  represented_country_geoname_id char(30),
-  is_anonymous_proxy char(30),
-  is_satellite_provider char(30),
-  postal_code char(30),
-  latitude char(10),
-  longitude char(10)
+  network varchar(18) NOT NULL,
+  geoname_id char(10) NOT NULL,
+  registered_country_geoname_id char(30) NOT NULL,
+  represented_country_geoname_id char(30) NOT NULL,
+  is_anonymous_proxy char(30) NOT NULL,
+  is_satellite_provider char(30) NOT NULL,
+  postal_code char(30) NOT NULL,
+  latitude char(10) NOT NULL,
+  longitude char(10) NOT NULL
 );
 
 CREATE INDEX cindex_ipv4_network ON "threatmonitor".geoipdata_ipv4blocks_city(network);
@@ -27,19 +27,19 @@ COPY 2519918 OFFSET 2 RECORDS INTO "threatmonitor".geoipdata_ipv4blocks_city FRO
 
 DROP TABLE "threatmonitor".geoipdata_locations_city;
 CREATE TABLE "threatmonitor".geoipdata_locations_city (
-  geoname_id char(10),
-  locale_code char(2),
-  continent_code char(2),
-  continent_name char(15),
-  country_iso_code char(2),
-  country_name char(50),
-  subdivision_1_iso_code char(70),
-  subdivision_1_name char(50),
-  subdivision_2_iso_code char(70),
-  subdivision_2_name char(50),
-  city_name char(70),
-  metro_code char(30),
-  time_zone char(30)
+  geoname_id char(10) NOT NULL,
+  locale_code char(2) NOT NULL,
+  continent_code char(2) NOT NULL,
+  continent_name char(15) NOT NULL,
+  country_iso_code char(2) NOT NULL,
+  country_name char(50) NOT NULL,
+  subdivision_1_iso_code char(70) NOT NULL,
+  subdivision_1_name char(50) NOT NULL,
+  subdivision_2_iso_code char(70) NOT NULL,
+  subdivision_2_name char(50) NOT NULL,
+  city_name char(70) NOT NULL,
+  metro_code char(30) NOT NULL,
+  time_zone char(30) NOT NULL
 );
 
 CREATE INDEX cindex_country_geoname_id ON "threatmonitor".geoipdata_locations_city(geoname_id);
@@ -48,12 +48,12 @@ COPY 80006 OFFSET 2 RECORDS INTO "threatmonitor".geoipdata_locations_city FROM '
 
 DROP TABLE "threatmonitor".geoipdata_ipv4blocks_country;
 CREATE TABLE "threatmonitor".geoipdata_ipv4blocks_country (
-  network varchar(18),
-  geoname_id char(10),
-  registered_country_geoname_id char(30),
-  represented_country_geoname_id char(30),
-  is_anonymous_proxy char(30),
-  is_satellite_provider char(30)
+  network varchar(18) NOT NULL,
+  geoname_id char(10) NOT NULL,
+  registered_country_geoname_id char(30) NOT NULL,
+  represented_country_geoname_id char(30) NOT NULL,
+  is_anonymous_proxy char(30) NOT NULL,
+  is_satellite_provider char(30) NOT NULL
 );
 
 CREATE INDEX index_ipv4_network ON "threatmonitor".geoipdata_ipv4blocks_country(network);
@@ -62,25 +62,37 @@ COPY 169357 OFFSET 2 RECORDS INTO "threatmonitor".geoipdata_ipv4blocks_country F
 
 DROP TABLE "threatmonitor".geoipdata_locations_country;
 CREATE TABLE "threatmonitor".geoipdata_locations_country (
-  geoname_id char(10),
-  locale_code char(2),
-  continent_code char(2),
-  continent_name char(15),
-  country_iso_code char(2),
-  country_name char(50)
+  geoname_id char(10) NOT NULL,
+  locale_code char(2) NOT NULL,
+  continent_code char(2) NOT NULL,
+  continent_name char(15) NOT NULL,
+  country_iso_code char(2) NOT NULL,
+  country_name char(50) NOT NULL
 -- FOREIGN KEY (geoname_id) REFERENCES "geoipdata".geoipdata_ipv4blocks_country (index_geoname_id)
 );
 
 CREATE INDEX index_country_geoname_id ON "threatmonitor".geoipdata_locations_country(geoname_id);
 COPY 250 OFFSET 2 RECORDS INTO "threatmonitor".geoipdata_locations_country FROM '/data2/MaxMind/GeoLite2-Country-CSV_20150602/GeoLite2-Country-Locations-en.csv' USING DELIMITERS ',', '\n', '';
 
-plan SELECT network, locale_code, continent_code, continent_name, country_name, country_iso_code 
+plan SELECT continent_name, country_name 
 FROM "threatmonitor".geoipdata_ipv4blocks_country a 
-JOIN "threatmonitor".geoipdata_locations_country b 
+JOIN "threatmonitor".geoipdata_locations_country b
 ON (a.geoname_id = b.geoname_id) 
-WHERE network LIKE '23.%' 
-GROUP BY a.network, b.locale_code, b.continent_code, b.continent_name, b.country_name, b.country_iso_code
-LIMIT 100;
+JOIN "threatmonitor".ippacket c
+ON (a.network = LEFT(c.ip_dst, 7))
+WHERE network LIKE '216.58.208.%' 
+GROUP BY b.continent_name, b.country_name
+LIMIT 10;
+
+SELECT ip_dst, network, continent_name, country_name 
+FROM "threatmonitor".geoipdata_ipv4blocks_country a 
+JOIN "threatmonitor".geoipdata_locations_country b
+ON (a.geoname_id = b.geoname_id)
+JOIN "threatmonitor".ippacket c
+ON (a.network LIKE LEFT(c.ip_dst, 5))
+WHERE network LIKE '216.58.%' 
+GROUP BY b.continent_name, b.country_name, a.network, c.ip_dst
+LIMIT 10;
 
 -- SELECT network FROM "threatmonitor".geoipdata_ipv4blocks_country a JOIN WHERE network LIKE '23.%' LIMIT 5;
  
