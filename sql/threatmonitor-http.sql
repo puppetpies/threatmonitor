9630ba0f82aa34f5c08cd17bf5f952256e03b2c0
Bri in The Sky <brianh6854@googlemail.com>
2015-09-11 19:28:03 +0100

    Added user defined function JSON_SQUASH

--- sql/threatmonitor-http.sql
+++ sql/threatmonitor-http.sql
@@ -6,10 +6,26 @@ id INT GENERATED ALWAYS AS
            START WITH 0 INCREMENT BY 1
            NO MINVALUE NO MAXVALUE
            CACHE 2 CYCLE
-) primary key,
-  guid char(36),
-  recv_date date,
-  recv_time time,
+) PRIMARY KEY,
+  guid CHAR(36),
+  recv_date DATE,
+  recv_time TIME,
   json_data JSON
 );
 
+CREATE FUNCTION JSON_SQUASH(name string)
+RETURNS string
+BEGIN
+  RETURN REPLACE(REPLACE(REPLACE(name, '[\"', ''), '\"]', ''), '"', '');
+END;
+
+/*
+SELECT JSON_SQUASH(host) AS host, 
+JSON_SQUASH(referer) AS referer 
+FROM 
+(SELECT 
+json.filter(json_data, '$.http.host') AS host, 
+json.filter(json_data, '$.http.referer') AS referer 
+FROM http_traffic_json) AS origin
+LIMIT 10;
+*/
