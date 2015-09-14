9940872e72ae54d8181960472f21f80a0fb5ded5
Bri in The Sky <brianh6854@googlemail.com>
2015-09-14 15:58:01 +0100

    Version Bump

--- lib/thm/version.rb
+++ lib/thm/version.rb
@@ -3,9 +3,9 @@ module Thm #:nodoc:
   module VERSION #:nodoc:
 
     MAJOR = 0
-    MINOR = 3
-    TINY = 2
-    CODENAME = "Micel"
+    MINOR = 4
+    TINY = 5
+    CODENAME = "The Isnis"
 
     STRING = [MAJOR, MINOR, TINY].join('.')
 
