5ddeda9a39551ee93d4aa6b8407ab86fd458e039
Bri in The Sky <brianh6854@googlemail.com>
2015-09-17 19:17:08 +0100

    Added next revision for geocoding

--- lib/thm/dataservices/geolocation/geolocation.rb
+++ lib/thm/dataservices/geolocation/geolocation.rb
@@ -31,9 +31,9 @@ module Thm
     
     def self.define_component(name)
       name_func = name.to_sym
-      define_method(name_func) do |ip|
+      define_method(name_func) do |ip, geo|
         octets = formatinet(ip, 2)
-        geoquery = "SELECT count(*) as num FROM geoipdata_ipv4blocks_#{name_func} a "
+        geoquery = "SELECT COUNT(*) as num FROM geoipdata_ipv4blocks_#{name_func} a "
         geoquery << "JOIN geoipdata_locations_#{name_func} b ON (a.geoname_id = b.geoname_id) "
         geoquery << "WHERE network LIKE '#{octets}.%';"
         begin
@@ -45,17 +45,34 @@ module Thm
         end
         puts "Geo SELECT COUNT: #{geoquery}: Number: #{geocount}" if @geodebug == true
         if geocount > 0;
-          geoquery = "SELECT continent_name, #{name_func}_name FROM geoipdata_ipv4blocks_#{name_func} a "
+          geoquery = "SELECT "
+          if geo == false
+            geoquery << "continent_name, #{name_func}_name "
+          else
+            geoquery << "latitude, longitude "
+          end
+          geoquery << "FROM geoipdata_ipv4blocks_#{name_func} a "
           geoquery << "JOIN geoipdata_locations_#{name_func} b ON (a.geoname_id = b.geoname_id) "
-          geoquery << "WHERE network LIKE '#{octets}.%' GROUP BY b.continent_name, b.#{name_func}_name LIMIT 1;"
+          geoquery << "WHERE network LIKE '#{octets}.%' GROUP BY "
+          if geo == false
+            geoquery << "b.continent_name, b.#{name_func}_name "
+          else
+            geoquery << "a.latitude, a.longitude "
+          end
+          geoquery << "LIMIT 1;"
           puts "Geo SELECT: #{geoquery}" if @geodebug == true
           begin
             resgeo = @conn.query("#{geoquery}")
             while row = resgeo.fetch_hash do
-              populategeo = instance_variable_get("@#{name_func}_name")
-              populategeo << row["#{name_func}_name"].to_s
-              instance_variable_set("@#{name_func}_name", populategeo) # Only returns 1 row
-              @continent_name = row["continent_name"].to_s
+              if geo == false
+                populategeo = instance_variable_get("@#{name_func}_name")
+                populategeo << row["#{name_func}_name"].to_s
+                instance_variable_set("@#{name_func}_name", populategeo) # Only returns 1 row
+                @continent_name = row["continent_name"].to_s
+              else
+                instance_variable_set("@latitude", row["latitude"].to_s)
+                instance_variable_set("@longitude", row["longitude"].to_s)
+              end
             end
           rescue => e
             pp e
@@ -69,18 +86,24 @@ module Thm
     define_component :country
     define_component :city
 
-    def geoiplookup(ip)
-      t = country(ip)
-      city(ip)
+    # Geo set to false by default for normal operation
+    def geoiplookup(ip, geo=false)
+      t = country(ip, geo)
+      city(ip, geo)
       unless t == false
-        res = "(#{@continent_name}) - \n"
-        @country_name.each {|n|
-          res << "[ #{n} ]"
-        }
-        @city_name.each {|n|
-          res << "[ #{n} ] "
-        }
-        initialize
+        # Check if @longitude / @latitude exists for Reverse Geocoding options
+        if instance_variable_defined?("@latitude") and instance_variable_defined?("@longitude")
+          res = [@latitude, @longitude]
+        else
+          res = "(#{@continent_name}) - \n"
+          @country_name.each {|n|
+            res << "[ #{n} ]" unless n == ""
+          }
+          @city_name.each {|n|
+            res << "[ #{n} ] " unless n == ""
+          }
+          initialize
+        end
       else
         res = "Not Available"
       end
@@ -89,4 +112,8 @@ module Thm
 
   end
 
+  class DataServices::Geocoding < DataServices
+  
+  end
+  
 end
