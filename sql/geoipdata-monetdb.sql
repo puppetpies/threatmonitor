64025aa40cd23fabc36657bb9cc1fa88eabffcd7
Bri in The Sky <brianh6854@googlemail.com>
2015-11-30 23:19:15 +0000

    Added SQL for new GeoIP Data version 20151103

--- sql/geoipdata-monetdb.sql
+++ sql/geoipdata-monetdb.sql
@@ -1,80 +1,75 @@
 -- http://dev.maxmind.com/geoip/geoip2/geolite2/
 
--- id INT GENERATED ALWAYS AS 
---        IDENTITY (
---           START WITH 1 INCREMENT BY 1
---           NO MINVALUE NO MAXVALUE
---           CACHE 2 CYCLE
--- ) primary key,
-
 SET SCHEMA "threatmonitor";
 
 DROP TABLE "threatmonitor".geoipdata_ipv4blocks_city;
 CREATE TABLE "threatmonitor".geoipdata_ipv4blocks_city (
-  network varchar(18) NOT NULL,
-  geoname_id char(10) NOT NULL,
-  registered_country_geoname_id char(30) NOT NULL,
-  represented_country_geoname_id char(30) NOT NULL,
-  is_anonymous_proxy char(30) NOT NULL,
-  is_satellite_provider char(30) NOT NULL,
-  postal_code char(30) NOT NULL,
-  latitude char(10) NOT NULL,
-  longitude char(10) NOT NULL
+  network varchar(18),
+  geoname_id char(10),
+  registered_country_geoname_id char(30),
+  represented_country_geoname_id char(30),
+  is_anonymous_proxy char(30),
+  is_satellite_provider char(30),
+  postal_code char(30),
+  latitude char(10),
+  longitude char(10)
 );
 
 CREATE INDEX cindex_ipv4_network ON "threatmonitor".geoipdata_ipv4blocks_city(network);
 CREATE INDEX cindex_ipv4_geoname_id ON "threatmonitor".geoipdata_ipv4blocks_city(geoname_id);
-COPY 3030997 OFFSET 2 RECORDS INTO "threatmonitor".geoipdata_ipv4blocks_city FROM '/home/brian/Downloads/GeoLite2-City-CSV_20151006/GeoLite2-City-Blocks-IPv4.csv' USING DELIMITERS ',', '\n', '';
+COPY 3037321 OFFSET 2 RECORDS INTO "threatmonitor".geoipdata_ipv4blocks_city FROM '/example_data/GeoLite2-City-CSV_20151103/GeoLite2-City-Blocks-IPv4.csv' USING DELIMITERS ',', '\n', '';
 
 
 DROP TABLE "threatmonitor".geoipdata_locations_city;
 CREATE TABLE "threatmonitor".geoipdata_locations_city (
-  geoname_id char(10) NOT NULL,
-  locale_code char(2) NOT NULL,
-  continent_code char(2) NOT NULL,
-  continent_name char(15) NOT NULL,
-  country_iso_code char(2) NOT NULL,
-  country_name char(50) NOT NULL,
-  subdivision_1_iso_code char(70) NOT NULL,
-  subdivision_1_name char(50) NOT NULL,
-  subdivision_2_iso_code char(70) NOT NULL,
-  subdivision_2_name char(50) NOT NULL,
-  city_name char(70) NOT NULL,
-  metro_code char(30) NOT NULL,
-  time_zone char(30) NOT NULL
+  geoname_id char(10),
+  locale_code char(2),
+  continent_code char(2),
+  continent_name char(15),
+  country_iso_code char(2),
+  country_name char(50),
+  subdivision_1_iso_code char(70),
+  subdivision_1_name char(50),
+  subdivision_2_iso_code char(70),
+  subdivision_2_name char(50),
+  city_name char(70),
+  metro_code char(30),
+  time_zone char(30)
 );
 
+-- You need to do a replace on the file for Quotes cat filename.csv | sed -e 's/"//g' > outputfile.csv
 CREATE INDEX cindex_country_geoname_id ON "threatmonitor".geoipdata_locations_city(geoname_id);
-COPY 91509 OFFSET 2 RECORDS INTO "threatmonitor".geoipdata_locations_city FROM '/home/brian/Downloads/GeoLite2-City-CSV_20151006/GeoLite2-City-Locations-en.csv' USING DELIMITERS ',', '\n', '';
+COPY 92781 OFFSET 2 RECORDS INTO "threatmonitor".geoipdata_locations_city FROM '/example_data/GeoLite2-City-CSV_20151103/GeoLite2-City-Locations-en.csv' USING DELIMITERS ',', '\n', '';
 
 
 DROP TABLE "threatmonitor".geoipdata_ipv4blocks_country;
 CREATE TABLE "threatmonitor".geoipdata_ipv4blocks_country (
-  network varchar(18) NOT NULL,
-  geoname_id char(10) NOT NULL,
-  registered_country_geoname_id char(30) NOT NULL,
-  represented_country_geoname_id char(30) NOT NULL,
-  is_anonymous_proxy char(30) NOT NULL,
-  is_satellite_provider char(30) NOT NULL
+  network varchar(18),
+  geoname_id char(10),
+  registered_country_geoname_id char(30),
+  represented_country_geoname_id char(30),
+  is_anonymous_proxy char(30),
+  is_satellite_provider char(30)
 );
 
 CREATE INDEX index_ipv4_network ON "threatmonitor".geoipdata_ipv4blocks_country(network);
 CREATE INDEX index_ipv4_geoname_id ON "threatmonitor".geoipdata_ipv4blocks_country(geoname_id);
-COPY 178589 OFFSET 2 RECORDS INTO "threatmonitor".geoipdata_ipv4blocks_country FROM '/home/brian/Downloads/GeoLite2-Country-CSV_20151006/GeoLite2-Country-Blocks-IPv4.csv' USING DELIMITERS ',', '\n', '';
+COPY 180336 OFFSET 2 RECORDS INTO "threatmonitor".geoipdata_ipv4blocks_country FROM '/example_data/GeoLite2-Country-CSV_20151103/GeoLite2-Country-Blocks-IPv4.csv' USING DELIMITERS ',', '\n', '';
 
 DROP TABLE "threatmonitor".geoipdata_locations_country;
 CREATE TABLE "threatmonitor".geoipdata_locations_country (
-  geoname_id char(10) NOT NULL,
-  locale_code char(2) NOT NULL,
-  continent_code char(2) NOT NULL,
-  continent_name char(15) NOT NULL,
-  country_iso_code char(2) NOT NULL,
-  country_name char(50) NOT NULL
+  geoname_id char(10),
+  locale_code char(2),
+  continent_code char(2),
+  continent_name char(15),
+  country_iso_code char(2),
+  country_name char(50)
 -- FOREIGN KEY (geoname_id) REFERENCES "geoipdata".geoipdata_ipv4blocks_country (index_geoname_id)
 );
 
+-- Fix the second to last line with 7626844,en,NA,North America,BQ,Bonaire Sint Eustatius and Saba
 CREATE INDEX index_country_geoname_id ON "threatmonitor".geoipdata_locations_country(geoname_id);
-COPY 250 OFFSET 2 RECORDS INTO "threatmonitor".geoipdata_locations_country FROM '/home/brian/Downloads/GeoLite2-Country-CSV_20151006/GeoLite2-Country-Locations-en.csv' USING DELIMITERS ',', '\n', '';
+COPY 250 OFFSET 2 RECORDS INTO "threatmonitor".geoipdata_locations_country FROM '/example_data/GeoLite2-Country-CSV_20151103/GeoLite2-Country-Locations-en.csv' USING DELIMITERS ',', '\n', '';
 
 plan SELECT continent_name, country_name 
 FROM "threatmonitor".geoipdata_ipv4blocks_country a 
